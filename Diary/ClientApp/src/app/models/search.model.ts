dfe73f3124c9cffdb72612edae2ed4eb35d7c8a4
Anton-crypto <52647224+Anton-crypto@users.noreply.github.com>
2022-05-23 07:24:36 +0300

    #comment update search component

---
+++ Diary/ClientApp/src/app/models/search.model.ts
@@ -0,0 +1,8 @@
+export interface ISearch {
+    bodySearch: string
+    tegs : string
+    rating : string
+    nameAuthor : string
+    typePost : string
+    dataType : string
+}
--- Diary/ClientApp/src/app/search/search.component.ts
+++ Diary/ClientApp/src/app/search/search.component.ts
@@ -1,6 +1,6 @@
 import { Component, Input, OnInit, Output,EventEmitter } from '@angular/core'
 import { UserService } from '../service/user.service';
-import { IUser } from '../models/user.model';
+import { ISearch } from '../models/search.model';
 import { ActivatedRoute } from '@angular/router';
 import { IPost } from '../models/post.model';
 import { PostService } from '../service/post.service';
@@ -14,11 +14,30 @@ import { PostService } from '../service/post.service';
 
 export class SearchComponent implements OnInit{
 
+  dateString : string = ""
+  date : Date | undefined
+  bodySearch : string = ""
+  author : string = ""
+
+  searchBody : ISearch = {
+    bodySearch: "",
+    tegs : "",
+    rating : "",
+    nameAuthor : "",
+    typePost : "",
+    dataType : "",
+  }
+
   constructor (
     private userService: UserService,
     private route: ActivatedRoute,
     private postService: PostService,
-  ){ }
+  ){ 
+
+    this.dateString =  new Date().toLocaleDateString();
+    this.date = new Date()
+    this.searchSet[4].types[3].value  = this.dateString; 
+  }
 
   @Input() posts: IPost[] = [];
 
@@ -26,6 +45,7 @@ export class SearchComponent implements OnInit{
     {
       name : "Теги",
       value : "",
+      tegs : [],
       isVisibel: false,
     },
     {
@@ -42,16 +62,59 @@ export class SearchComponent implements OnInit{
       name : "Тип постов",
       value : "",
       isVisibel: false,
+      types : [
+        {
+          name: "любые",
+          isActive: false,
+        },
+        {
+          name: "текстовые",
+          isActive: false,
+        },
+        {
+          name: "картинка",
+          isActive: false,
+        },
+        {
+          name: "видео",
+          isActive: false,
+        },
+        {
+          name: "[ мое ]",
+          isActive: false,
+        },
+        {
+          name: "NSFW",
+          isActive: false,
+        }
+      ]
     },
     {
       name : "Период времени",
       value : "",
       isVisibel: false,
-    },
-    {
-      name : "Просмотренные посты",
-      value : "",
-      isVisibel: false,
+      types : [
+        {
+          name: "за все время",
+          isActive: true,
+          value : "max"
+        },
+        {
+          name: "неделя",
+          isActive: false,
+          value : "7"
+        },
+        {
+          name: "месяц",
+          isActive: false,
+          value : "30"
+        },
+        {
+          name: "задать даты",
+          isActive: false,
+          value : ""
+        },
+      ]
     },
   ]
 
@@ -64,7 +127,88 @@ export class SearchComponent implements OnInit{
       posts : this.posts
     })
   }
+  serAuthorName(event : any) {
+    console.log(event.target.value)
+  }
   setRam (event : any) {
     this.searchSet[1].value = event
+    this.searchBody.rating = event
+  }
+  addTeg (event : any) {
+    if(event.keyCode !== 13 ) return;
+    if(this.searchSet[0].value.length == 0) return;
+
+    this.searchSet[0].tegs.push(this.searchSet[0].value)
+    this.searchSet[0].value = ""
+  }
+  deleteTeg (index : any) {
+    this.searchSet[0].tegs.splice(index, 1);
+  }
+  activeTypePost(index: number) {
+    let item = this.searchSet[3].types[index];
+
+    if(item.name == "любые" && item.isActive == false) {
+      this.searchSet[3].types.forEach((element : any) => {
+        element.isActive = false;
+      });
+    } 
+    if(item.name != "любые") {
+      this.searchSet[3].types[0].isActive = false;
+    } 
+
+    item.isActive = !item.isActive;
+  }
+  getClassForButtonsType(isActive : boolean ) : string {
+    if(isActive == true) {
+      return "activeType"
+    }
+    return ""
+  }
+  setDateRange(index: number) {
+    this.searchSet[4].types[index].isActive = true;
+    this.searchBody.dataType = this.searchSet[4].types[index].name // ISearch
+
+    this.searchSet[4].types.forEach((element : any , indexFor : number) => {
+      if(indexFor == index) return;
+      element.isActive = false;
+    });
+  }
+  setDate(event : any) {
+    this.dateString = new Date(event.target.valueAsDate).toLocaleDateString()
+
+    this.searchSet[4].types[3].isActive = true;
+    this.searchSet[4].types[3].value = this.dateString; 
+    this.searchBody.dataType = this.dateString; // ISearch
+
+    this.searchSet[4].types.forEach((element : any , indexFor : number) => {
+      if(indexFor == 3) return;
+      element.isActive = false;
+    });
+  }
+
+  getPostForSearch() {
+    this.searchBody.bodySearch = this.bodySearch
+    this.searchBody.nameAuthor = this.author
+    this.searchBody.tegs = ""
+    this.searchBody.typePost = ""
+    this.searchBody.dataType = ""
+
+    this.searchSet[3].types.forEach((element : any) => {
+      if(element.isActive) {
+        this.searchBody.typePost += element.name + ","
+      }
+    });
+
+    this.searchSet[4].types.forEach((element : any) => {
+      if(element.isActive) {
+        this.searchBody.dataType = element.value
+      }
+    });
+
+    this.searchSet[0].tegs.forEach((element : any) => {
+      this.searchBody.tegs += element + ","
+    });
+
+    console.log(this.searchBody)
   }
 }
